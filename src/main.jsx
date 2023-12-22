aa459fab84672240093e508a460d5e97d9e055f2
AngelSuarez03 <angnusa11@gmail.com>
2023-12-21 20:29:58 -0600

    changes hashroute

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createHashRouter, RouterProvider} from "react-router-dom"
+
+import Login from './Login/Login.jsx'
+import SingUp from './SingUp/SignUp.jsx'
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element:<Login/>
+  },
+  {
+    path: "/SingUp",
+    element:<SingUp/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
